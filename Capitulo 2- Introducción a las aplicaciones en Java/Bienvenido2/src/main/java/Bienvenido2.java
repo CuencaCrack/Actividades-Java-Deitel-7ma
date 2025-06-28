135f2369d3250f52050624274cbf0e67b4c05b6d
Marcos Aldahir <cuencamarcos44@gmail.com>
2025-06-28 00:26:05 -0600

    Agregando actividad 2.3

---
+++ Capitulo 2- Introducción a las aplicaciones en Java/Bienvenido2/src/main/java/Bienvenido2.java
@@ -0,0 +1,14 @@
+/*Figura 2.3 Bienvenido2.java
+ * Imprimir una linea e texto con varias instrucciones
+ * Como programar en Java Deitel & Deitel 7ma Edición
+ * */
+
+public class Bienvenido2 {
+    //El método main empieza la ejecución del programa de la aplicación Java
+    public static void main(String[] args) {
+
+        System.out.print("Bienvenido a ");
+        System.out.println("la progrmación en Java!");
+
+    }//Fin del método main
+}//Fin de la Clase Bienvenido 2
